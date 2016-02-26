64e45645ce303dd9d60b6d49e4acae443796e9ba
shajeer <shajeer.ahamed@robosoftin.com>
2016-02-26 14:30:37 +0530

    models added

---
+++ app/src/main/java/com/droidev/util/expensetracker/intf/ApiService.java
@@ -0,0 +1,21 @@
+package com.droidev.util.expensetracker.intf;
+
+import com.droidev.util.expensetracker.model.GenericResponse;
+import com.droidev.util.expensetracker.model.UserLogin;
+import com.droidev.util.expensetracker.model.UserRegistration;
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.POST;
+
+/**
+ * Created by shajeer on 25/2/16.
+ */
+public interface ApiService {
+
+    @POST("user/login")
+    void startLogin(@Body UserLogin userLogin, Callback<GenericResponse> callback);
+
+    @POST("user/registration")
+    void startRegistration(@Body UserRegistration registration, Callback<GenericResponse> callback);
+
+}
---
+++ app/src/main/java/com/droidev/util/expensetracker/model/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.droidev.util.expensetracker.model;
+
+/**
+ * Created by shajeer on 25/2/16.
+ */
+public class UserRegistration {
+}
